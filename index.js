37f62feb312d597c9aa793eaae95200f48c0a2d0
Chance Hudson <jchancehud@gmail.com>
2019-08-16 13:28:28 -0500

    don't pre-emptively call functions

--- index.js
+++ index.js
@@ -5,18 +5,16 @@
  *
  * Second argument is max parallel execution (default 10)
  **/
-export default (count, iteratorFn, limit = 10) => {
+module.exports = (count, iteratorFn, limit = 10) => {
   const targetPromises = Array.apply(null, Array(limit)).map(() =>
     Promise.resolve()
   )
-  const results: any[] = new Array(count)
+  const results = new Array(count)
   for (let i = 0; i < count; i += 1) {
-    const promise = iteratorFn(i)
-    if (promise === false) break
     const index = i % targetPromises.length
     targetPromises[index] = targetPromises[index]
-      .then(() => promise)
-      .then((r: any) => (results[i] = r))
+      .then(() => (() => iteratorFn(i))())
+      .then((r) => (results[i] = r))
   }
   return Promise.all(targetPromises).then(() => results)
 }
